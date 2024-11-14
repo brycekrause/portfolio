7ad3306d856a5031dbef7d4948486357a5cd66e6
imjustjayk <brycekrause@icloud.com>
2024-11-13 19:08:26 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+let education = document.getElementById("education");
+let experience = document.getElementById("experience");
+
+let edu_button = document.getElementById("edu-btn");
+let exp_button = document.getElementById("exp-btn");
+
+education.style.display = 'none';
+let selected = 'exp';
+
+if (education.style.display == 'none'){
+    exp_button.style.backgroundColor = 'white';
+    exp_button.style.color = 'black';
+}else if (experience.style.display == 'none'){
+    edu_button.style.backgroundColor = 'white';
+    edu_button.style.color = 'black';
+}
+
+edu_button.addEventListener('click', function(){
+    experience.style.display = 'none';
+    education.style.display = 'block';
+
+    edu_button.style.backgroundColor = 'white';
+    edu_button.style.color = 'black';
+
+    exp_button.style.backgroundColor = '#2D2E2E';
+    exp_button.style.color = 'white';
+
+    selected = 'edu';
+
+    edu_button.onmouseover = function(){
+        if (selected == 'edu'){
+        }else{
+            edu_button.style.backgroundColor = 'white';
+            edu_button.style.color = 'black';        
+        }
+    };
+    edu_button.onmouseleave = function(){
+        if (selected == 'edu'){
+        }else{
+            edu_button.style.backgroundColor = '#2D2E2E';
+            edu_button.style.color = 'white';   
+        };
+  
+    };
+});
+exp_button.addEventListener('click', function(){
+    education.style.display = 'none';
+    experience.style.display = 'block';
+
+    exp_button.style.backgroundColor = 'white';
+    exp_button.style.color = 'black';
+
+    edu_button.style.backgroundColor = '#2D2E2E';
+    edu_button.style.color = 'white';
+
+    selected = 'exp';
+
+    exp_button.onmouseover = function(){
+        if (selected == 'exp'){
+        }else{
+            exp_button.style.backgroundColor = 'white';
+            exp_button.style.color = 'black';        
+        }
+    };
+    
+    exp_button.onmouseleave = function(){
+        if (selected == 'exp'){
+        }else{
+            exp_button.style.backgroundColor = '#2D2E2E';
+            exp_button.style.color = 'white';   
+        };
+    };
+});
